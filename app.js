ace4fc93f635751768496c44d188e88252e6645d
hook2000 <elias@elias.ninja>
2021-08-07 12:46:59 +0200

    got mysql to work

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.send('Hello World! <a href="/connect">Connect</a>');
+});
+
+const db = mysql.createConnection({
+	host: 'db',
+	user: 'elias',
+	password: 'secret',
+	database: 'test_db',
+});
+app.get('/connect', (req, res) => {
+	db.connect(function (err) {
+		if (err) {
+			console.error('error connecting: ' + err.stack);
+			return;
+		}
+
+		res.send('connected as id ' + db.threadId);
+	});
 });
 
 app.listen(3000, () => {
